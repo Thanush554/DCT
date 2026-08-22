1329a255902f642772d1f639919f5a5e42858d3e
Thanush554 <thanushgowda554@gmail.com>
2026-08-22 14:16:08 +0530

    added variable

--- Javascript/name.js
+++ Javascript/name.js
@@ -1,1 +1,16 @@
 console.log("Thanush g gowda");
+
+// camel casing
+myName="Thanush g gowda";
+console.log(myName);
+
+myCity="bengaluru";
+console.log(myCity);
+
+myAge=20;
+console.log(myAge);
+
+// snake casing
+my_city="mysuru";
+console.log(my_city);
+
---
+++ Javascript/variable.js
@@ -0,0 +1,17 @@
+//variable initilization
+const myName="Thanush G Gowda";
+console.log(myName);
+
+//variable reassignment
+// will throw type error because it will not allow for modification
+// myName="thanush";
+// console.log(myName)
+
+//using let
+let temp=26.6;
+console.log(temp);
+
+//reassignment
+// it will not show any error because it will allow for modification
+temp=30.5;
+console.log(temp);
