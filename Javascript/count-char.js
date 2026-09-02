17be881f4634535962bb01f3cea6b2e885a715a1
Thanush554 <thanushgowda554@gmail.com>
2026-09-02 16:25:30 +0530

    started function

--- Javascript/count-char.js
+++ Javascript/count-char.js
@@ -1,8 +1,21 @@
-const str="javascript";
-let count=0;
-for(let i=0;i<str.length;i++){
-    if(str.charAt(i)=="a"){
-        count+=1;
+// const str="javascript";
+// let count=0;
+// for(let i=0;i<str.length;i++){
+//     if(str.charAt(i)=="a"){
+//         count+=1;
+//     }
+// }
+// console.log(count);
+
+function countChar(str, ch){
+    let count=0;
+    for(let i=0;i<str.length;i++){
+        if(str[i]==ch){
+            count+=1;
+        }
     }
+    return count;
 }
-console.log(count);
+console.log(countChar("google","g"));
+console.log(countChar("facebook", "a"));
+console.log(countChar("facebook", "t"));
---
+++ Javascript/reversestrfunction.js
@@ -0,0 +1,9 @@
+function reverseMystring(str){
+    let result="";
+    for(let i=str.length-1;i>=0;i--){
+        result+=str[i];
+    }
+    return result;
+}
+console.log(reverseMystring("google"));
+console.log(reverseMystring("claude"));
